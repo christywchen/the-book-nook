b1aab0bcc3e99e8b3f2ef22f42cdda5acf8b616c
Christy Chen <christy.w.chen@gmail.com>
2022-02-18 02:10:13 -0800

    cleanup on book club card.

--- react-app/src/components/BookClubs/BookClubCard/BookClubCard.js
+++ react-app/src/components/BookClubs/BookClubCard/BookClubCard.js
@@ -14,63 +14,34 @@ function BookClubCard({ bookClub }) {
     const backgroundImage = { backgroundImage: `url("${image_url}")` }
 
     const sessionUser = useSelector(state => state.session.user);
-
     const userMembershipsObj = useSelector(state => state.bookClubMember.userMembershipsByClubId);
+    const bookClubMembersObj = useSelector(state => state.bookClubMember.allMembershipsByClubId[bookClub.id]);
+
     const userMemberships = Object.values(userMembershipsObj);
     const userMembership = userMembershipsObj[id];
 
-    const [membershipCount, setMembershipCount] = useState(userMemberships.length);
     const [buttonText, setButtonText] = useState(userMembership ? 'Go to Club' : 'Join Now');
-    const [disableButton, setDisableButton] = useState(false);
 
-    const allUsersObj = useSelector(state => state.user.byUserId);
-
-    const bookClubMembersObj = useSelector(state => state.bookClubMember.allMembershipsByClubId[bookClub.id]);
-
-    console.log(bookClubMembersObj, 'MEMBERS OBJ')
     let memberCount;
     if (bookClubMembersObj) memberCount = Object.values(bookClubMembersObj).length;
-    // const [availableSpace, setAvailableSpace] = useState(bookClub.capacity - memberCount)
 
-    console.log(bookClub.id, bookClub.capacity, memberCount)
     const availableSpace = bookClub.capacity - memberCount;
 
     useEffect(() => {
-        // dispatch(getAllBookClubMem)
         dispatch(getBookClubMembers(id));
         dispatch(getUserMemberships(sessionUser.id));
     }, [dispatch]);
 
     useEffect(() => { }, [availableSpace, memberCount]);
 
-    console.log(membershipCount, 'MEMBERHSIP COUNT BEFORE')
-
-    console.log(availableSpace, 'avaible space before')
     async function handleMembership(e) {
         e.preventDefault();
 
-        if (buttonText == 'Go to Club') {
-            // await dispatch(deleteBookClubMember(bookClub.id, sessionUser.id));
-            // await dispatch(removeUserMembership(bookClub.id));
-            // setButtonText('Join Club');
-
-            // console.log('left club')
-
-            return history.push(`/dashboard/book-clubs/${bookClub.id}`);
-        }
-
         if (buttonText == 'Join Now') {
             const membership = await dispatch(createBookClubMember(bookClub.id, sessionUser.id));
-            // await dispatch(getUserMemberships(sessionUser.id));
             await dispatch(addUserMembership(membership));
-
-            setButtonText('Leave Club');
-
-            console.log('joined club')
         }
         return history.push(`/dashboard/book-clubs/${bookClub.id}`);
-        console.log(membershipCount, 'AFTERMEMBERSHIP COUNT')
-        console.log(availableSpace, 'avaible space after')
     }
 
     return (
--- react-app/src/components/Sidebar/BrowseClubs/BrowseClubs.js
+++ react-app/src/components/Sidebar/BrowseClubs/BrowseClubs.js
@@ -12,7 +12,7 @@ function BrowseClubs({ membershipCount }) {
         <>
             <div className='sidebar__para'>
                 <p>
-                    Joining a book club is easy! You can join or create up to 5 book clubs.
+                    Joining a book club is easy! You can join or create up to 5 book clubs at a time.
                 </p>
                 <p>
                     Once you find one that you like, click to join and get to chatting with your new book club members!
