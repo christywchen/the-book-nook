42a5bc7f639d8b6114095aeb5451d944649c502e
Christy Chen <christy.w.chen@gmail.com>
2022-03-17 15:15:19 -0700

    set up search form.

--- react-app/src/components/Forms/SearchBooks/SearchBooks.js
+++ react-app/src/components/Forms/SearchBooks/SearchBooks.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+
+import './SearchBooks.css';
+
 function SearchBooks() {
+    const [query, setQuery] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        console.log('send search')
+    }
+
+    async function handleQuery(e) {
+        setQuery(e.target.value);
+        console.log('query a thing')
+    }
+
     return (
         <>
-
+            <form id='search__form' onSubmit={handleSubmit}>
+                {/* <div id='search__form'> */}
+                <label form='search'>
+                    <input
+                        name='search'
+                        type='text'
+                        value={query}
+                        placeholder='Search books...'
+                        onChange={handleQuery} />
+                </label>
+                <span>
+                    <button type='submit'>Search</button>
+                </span>
+                {/* </div> */}
+            </form>
         </>
     )
 }
